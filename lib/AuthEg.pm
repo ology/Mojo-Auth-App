8c461890534bc39967a670c3d4319d3e8c602472
Gene Boggs <gene@ology.net>
2022-12-22 14:15:28 -0800

    Plugins together

--- lib/AuthEg.pm
+++ lib/AuthEg.pm
@@ -4,9 +4,9 @@ use Mojo::Base 'Mojolicious', -signatures;
 sub startup ($self) {
   my $config = $self->plugin('NotYAMLConfig');
 
-  $self->secrets($config->{secrets});
-
   $self->plugin('AuthEg::DB::Plugin');
+
+  $self->secrets($config->{secrets});
 
   my $r = $self->routes;
   $r->get('/')      ->to('Main#index') ->name('index');
