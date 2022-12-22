96d8068d5b3f2278ed4c536d101979653e5730ac
Gene Boggs <gene@ology.net>
2022-12-22 14:23:54 -0800

    Whitespace

--- lib/AuthEg/DB/Plugin.pm
+++ lib/AuthEg/DB/Plugin.pm
@@ -4,24 +4,24 @@ use Mojo::Base 'Mojolicious::Plugin';
 use AuthEg::DB::Schema;
 
 sub register {
-    my ( $self, $app ) = @_;
+    my ($self, $app) = @_;
 
-    $app->helper( schema => sub {
+    $app->helper(schema => sub {
         my ($c) = @_;
         return state $schema = AuthEg::DB::Schema->connect('db');
-    } );
+    });
 
-    $app->helper( auth => sub {
-        my ( $c, $user, $pass ) = @_;
+    $app->helper(auth => sub {
+        my ($c, $user, $pass) = @_;
 
         my $result = $c->schema->resultset('Account')->search({ name => $user })->first;
 
         return 1
             if $result && $result->check_password($pass);
-    } );
+    });
 
-    $app->helper( add => sub {
-        my ( $c, $user, $pass ) = @_;
+    $app->helper(add => sub {
+        my ($c, $user, $pass) = @_;
 
         return 0
             unless $user && $pass;
@@ -29,24 +29,24 @@ sub register {
         my $result = $c->schema->resultset('Account')->create({ name => $user, password => $pass });
 
         return $result;
-    } );
+    });
 
-    $app->helper( remove => sub {
-        my ( $c, $id ) = @_;
+    $app->helper(remove => sub {
+        my ($c, $id) = @_;
 
         my $result = $c->schema->resultset('Account')->search({ id => $id })->first;
         $result->delete if $result;
 
         return $result ? 1 : 0;
-    } );
+    });
 
-    $app->helper( list_accounts => sub {
+    $app->helper(list_accounts => sub {
         my ($c) = @_;
 
         my $result = $c->schema->resultset('Account');
 
         return $result;
-    } );
+    });
 
 }
 
