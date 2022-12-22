dd99f8c455ce97f2411e454b04ecfd93518479ed
Gene Boggs <gene@ology.net>
2022-12-22 00:14:08 -0800

    Add a remove route

--- lib/AuthEg.pm
+++ lib/AuthEg.pm
@@ -9,12 +9,13 @@ sub startup ($self) {
   $self->plugin('AuthEg::DB::Plugin');
 
   my $r = $self->routes;
-  $r->get('/')->to('Main#index')->name('index');
-  $r->post('/login')->to('Main#login')->name('login');
+  $r->get('/')      ->to('Main#index') ->name('index');
+  $r->post('/login')->to('Main#login') ->name('login');
   $r->get('/logout')->to('Main#logout')->name('logout');
   my $authed = $r->under('/authed')->to('Main#authorize');
   $authed->get('/accounts')->to('Main#accounts')->name('accounts');
-  $authed->post('/add')->to('Main#new_user')->name('new_user');
+  $authed->post('/add')    ->to('Main#new_user')->name('new_user');
+  $authed->get('/remove')  ->to('Main#remove')  ->name('remove');
 }
 
 1;
