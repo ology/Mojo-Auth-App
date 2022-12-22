b71b1ed71581d24ef53c3d30c9199e63e9c0d871
Gene Boggs <gene@ology.net>
2022-12-21 16:27:59 -0800

    Drop unused code

--- lib/AuthEg.pm
+++ lib/AuthEg.pm
@@ -1,24 +1,14 @@
 package AuthEg;
 use Mojo::Base 'Mojolicious', -signatures;
 
-use Mojolicious::Plugin::Bcrypt;
-
 sub startup ($self) {
-
-  # Load configuration from config file
   my $config = $self->plugin('NotYAMLConfig');
 
-  # Configure the application
   $self->secrets($config->{secrets});
 
-  $self->plugin('bcrypt', { key_nul => 0, cost => 6 });
-
-  # 
   $self->plugin('AuthEg::DB::Plugin');
 
-  # Router
   my $r = $self->routes;
-
   $r->get('/')->to('Main#index')->name('index');
   $r->post('/login')->to('Main#login')->name('login');
   $r->get('/logout')->to('Main#logout')->name('logout');
